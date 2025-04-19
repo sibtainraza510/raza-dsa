8e7856cbff8c266ddccec6875a7b9e99e522de8b
sibtayn <sibtaynrza54017@gmail.com>
2025-04-19 12:02:59 +0530

    complete string

---
+++ stringsfolder/string1.java
@@ -0,0 +1,105 @@
+package stringsfolder;
+import java.util.*;
+public class string1{
+    public static void main(String[] args) {
+        // String name = "muhammad sibtain raza sheikh";
+        // System.out.println(name);
+        // System.out.println(name.charAt(0));
+        // System.out.println(name.length());
+        // System.out.println(name.replace("muhammad", "sheikh"));
+
+        // name = "sibtain raza ";
+        // System.out.println(name);
+
+
+
+        // String name1 = "abcd";
+        // String name2 = "abcd";
+        // String c = name1;
+        // System.out.println(c == name1);
+        // System.out.println(name1 == name2);
+        // String name3 = new String("raza");
+        // String name4 = new String("raza");
+        // String name5 = name4;
+        // System.out.println(name5 == name4);
+        // System.out.println(name3 == name4);
+
+
+        //strings methods
+        // String name  = "muhammad raza sheikh";
+        // System.out.println(Arrays.toString(name.toCharArray()));
+        // System.out.println(name.toLowerCase());
+        // System.out.println(name.indexOf("d"));
+        // System.out.println("     Kunal   ".strip());
+        // System.out.println(Arrays.toString(name.split(" ")));
+
+        //operators;;
+        // System.out.println('a' + 'b');
+        // System.out.println("a" + "b");
+        // System.out.println((char)('a' + 3));
+        // System.out.println("a" + 1);
+        // this is same as after a few steps: "a" + "1"
+        // integer will be converted to Integer that will call toString()
+        // System.out.println("Kunal" + new ArrayList<>());
+        // System.out.println("Kunal" + 56);
+        // String ans = 56 + "" + new ArrayList<>();
+        // System.out.println(ans);
+        // System.out.println("a" + 'b');
+        // System.out.println(Arrays.toString(new int[]{2, 3, 4, 5}));
+
+
+        //characters;;
+
+        // String atoz  = "";
+        // for(int i=0;i<26;i++){
+        //     char ch = (char)('a' + i);
+        //     atoz = atoz + ch;
+        // }
+
+        // System.out.println(atoz);
+
+
+        //float;;
+        // float a = 123.4567890f;
+        // System.out.println(a);
+        // String name = "muhammad raza";
+        // System.out.println("my name is  " +  name);
+
+        // float a = 453.1274f;
+        //        System.out.printf("Formatted number is %.4f", a);
+        
+        //        System.out.printf("Pie: %.1f", Math.PI);
+        
+        //         System.out.printf("Hello my name is %s and I am %s", "Kunal", "Cool"); // or only concatenation
+
+
+
+        ////palindrome;;
+        // String name = "madam";
+        // String name2 = "";
+        // for(int i=name.length()-1;i>=0;i--){
+           
+        //     name2 = name2 + name.charAt(i);
+        // }
+        // System.out.println(name2);
+
+        // if(name2.equals(name)){
+        //     System.out.println("its a palindrome string ");
+
+        // }else{
+        //     System.out.println("not palindrome");
+        // }
+
+
+        //string builder;;
+        // StringBuilder builder = new StringBuilder();
+        // for(int i=0;i<26;i++){
+        //     char ch = (char)('a' + i);
+        //     builder.append(ch);
+        // }
+        // System.out.println(builder);
+        // System.out.println(builder.toString());
+        // System.out.println(builder.reverse());
+
+    }
+}
